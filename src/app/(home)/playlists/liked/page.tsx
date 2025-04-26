e71dd1f1f6b7ee6c165701bc17d34b29b49b2559
gitdeepaks <deepaksankhyan92@outlook.com>
2025-04-26 11:18:02 +0530

    fixed types and lint errors

--- src/app/(home)/playlists/liked/page.tsx
+++ src/app/(home)/playlists/liked/page.tsx
@@ -1,11 +1,20 @@
 import { DEFAULT_LIMIT } from "@/constants";
 import { LikedView } from "@/modules/playlists/ui/views/liked-view";
 import { HydrateClient, trpc } from "@/trpc/server";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 const Page = async () => {
+  const { userId } = await auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
   void trpc.playlists.getLiked.prefetchInfinite({
     limit: DEFAULT_LIMIT,
   });
+
   return (
     <HydrateClient>
       <LikedView />
--- src/app/(home)/subscriptions/page.tsx
+++ src/app/(home)/subscriptions/page.tsx
@@ -1,11 +1,20 @@
 import { DEFAULT_LIMIT } from "@/constants";
 import { SubscriptionsViewPage } from "@/modules/subscriptions/ui/views/subscriptions-view-page";
 import { HydrateClient, trpc } from "@/trpc/server";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 const SubscriptionsPage = async () => {
+  const { userId } = await auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
   void trpc.subscriptions.getMany.prefetchInfinite({
     limit: DEFAULT_LIMIT,
   });
+
   return (
     <HydrateClient>
       <SubscriptionsViewPage />
